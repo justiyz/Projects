024c1b2ecf5032b39a207731c916eb02f3920760
justiyz <inyange@yahoo.com>
2020-09-26 03:05:47 +0100

    Day-4

---
+++ ReversingNumbers.java
@@ -0,0 +1,33 @@
+package com.chapter6;
+
+import java.util.Scanner;
+
+public class ReversingNumbers {
+
+	public void getReverseNum() {
+		Scanner scanner = new Scanner(System.in);
+		int reversedNum = 0;
+		try {
+			System.out.println("Enter a any digit you want to reverse: ");
+			int number = scanner.nextInt();
+			reversedNum = 0;
+			while (number != 0) {
+				reversedNum = reversedNum * 10;
+				reversedNum = reversedNum + number % 10;
+				number = number / 10;
+			}
+			System.out.println(reversedNum);
+		} catch (Exception e) {
+			System.out.println("you have entered a non-integer value");
+		}
+
+		scanner.close();
+
+	}
+
+	public static void main(String[] args) {
+		ReversingNumbers rev = new ReversingNumbers();
+		rev.getReverseNum();
+	}
+
+}
